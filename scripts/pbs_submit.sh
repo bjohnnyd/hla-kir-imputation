00b630b20f121d9c53adb4091018d882e235b522
Becca Asquith <rea30@login-7.rcs.ic.ac.uk>
2019-10-19 11:49:55 +0100

    changed script for submission

--- scripts/pbs_submit.sh
+++ scripts/pbs_submit.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-# properties = {properties}
-conda activate hla-kir-imp
+#$ -cwd
+#$ -V
 
 python -c "import cyvcf2" 2> /dev/null || pip install cyvcf2==0.11.5
 python -c "import matplotlib"  2> /dev/null || pip install matplotlib==3.1.1
